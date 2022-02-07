5b99db66065e17fd000353d47441f6336e1307c4
John Bulla <chulth@gmail.com>
2022-02-07 06:04:06 -0500

    Create constants file

---
+++ project_habi/constants/constants.py
@@ -0,0 +1,21 @@
+FILTER_CITY_AND_YEAR_QUERY = (
+                "SELECT * FROM property where city = '{0}' "
+                "and year = {1};"
+                        )
+STATUS_ID_QUERY = (
+            "SELECT MAX(status_id) FROM "
+            " status_history where property_id = {0}"
+        )
+PROPERTY_QUERY = (
+                    "SELECT property.address, property.description, "
+                    "property.price, property.city, "
+                    "MAX(status_history.status_id) FROM  "
+                    "property, status_history where property.id={0} "
+                    " and property_id={0};"
+                    )
+FILTER_CITY_QUERY = (
+        "SELECT * FROM property where city = '{0}';"
+        )
+FILTER_YEAR_QUERY = (
+                "SELECT * FROM property where year = {0};"
+                )
